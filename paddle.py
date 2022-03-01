697ce3b57b3690e9017ca263614499de0611d961
changeAJay <ajajay008@gmail.com>
2022-03-01 09:49:37 +0530

    Adjusted paddle size and movement

--- paddle.py
+++ paddle.py
@@ -5,17 +5,17 @@ class Paddle(Turtle):
     def __init__(self, pos):
         super().__init__()
         self.shape('square')
-        self.shapesize(stretch_wid=4, stretch_len=1)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.color('white')
         self.penup()
         self.goto(pos, 0)
 
     def move_up(self):
         y = self.ycor()
-        if y < 250:
+        if y < 240:
             self.sety(y+20)
 
     def move_down(self):
         y = self.ycor()
-        if y > -250:
+        if y > -240:
             self.sety(y-20)
