660f73168fd1d29a8d0d50ffa2170ce2e96b2a1a
changeAJay <ajajay008@gmail.com>
2022-03-01 09:53:48 +0530

    Ball position reset and ball bounce

--- ball.py
+++ ball.py
@@ -4,21 +4,26 @@ from turtle import Turtle
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape('circle')
-        self.shapesize(stretch_wid=1, stretch_len=1)
         self.color('white')
+        self.shape('circle')
         self.penup()
-        self.goto(0, 0)
         self.x = 10
         self.y = 10
+        self.ball_speed = 0.1
 
     def move(self):
         y = self.ycor() + self.y
         x = self.xcor() + self.x
         self.goto(x, y)
 
-    def w_bounce(self):
+    def y_bounce(self):
         self.y *= -1
 
-    def p_bounce(self):
+    def x_bounce(self):
         self.x *= -1
+        self.ball_speed *= 0.9
+
+    def reposition(self):
+        self.goto(0, 0)
+        self.ball_speed = 0.1
+        self.x_bounce()
