afcb45264c2cadc084171df3aed97763a235c4fa
changeAJay <ajajay008@gmail.com>
2022-03-01 09:56:35 +0530

    1. Ball collision with the paddle and wall    i. Right paddle miss   ii. Left paddle miss

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
 import time
-import turtle
-import random
+from scoreboard import ScoreBoard
+from separate import Separate
+
 
 s = Screen()
 s.setup(width=800, height=600)
@@ -11,21 +12,11 @@ s.bgcolor('black')
 s.title('Pong')
 s.tracer(0)
 
-# turtle.colormode(255)
-# t = Turtle()
-# t.penup()
-# t.setheading(90)
-# t.forward(280)
-# t.setheading(270)
-#
-# for i in range(30):
-#     t.dot(15, (255, 255, 255))
-#     t.shapesize(stretch_wid=2, stretch_len=0.5)
-#     t.forward(20)
-
 p1 = Paddle(360)
 p2 = Paddle(-360)
 b = Ball()
+score = ScoreBoard()
+line = Separate()
 
 
 s.listen()
@@ -37,17 +28,26 @@ s.onkey(key='s', fun=p2.move_down)
 game = True
 
 while game:
+    time.sleep(b.ball_speed)
     s.update()
-    time.sleep(0.1)
     b.move()
 
+    #when the ball touches the above wall it bounces back in opp direction
     if b.ycor() > 280 or b.ycor() < -280:
-        b.w_bounce()
+        b.y_bounce()
 
-    if b.distance(p1) < 50 and b.xcor() > 340 or b.distance(p2) < 50 or b.xcor() < -340:
-        b.p_bounce()
+    #when a paddle hit the ball it bounces back in opp direction
+    if b.distance(p1) < 60 and b.xcor() > 330 or b.distance(p2) < 60 and b.xcor() < -330:
+        b.x_bounce()
 
-    if b.xcor() > 395 or b.xcor() < -385:
-        game = False
+    #Right paddle miss
+    if b.xcor() > 380:
+        b.reposition()
+        score.update_leftscore()
+
+    #Left paddle miss
+    if b.xcor() < -395:
+        b.reposition()
+        score.update_rightscore()
 
 s.exitonclick()
